769c42cd1ea1eec5343846c01b966ac82b67bb49
Serenay <serenayygunes@gmail.com>
2023-10-13 17:54:24 +0300

    VillaAPIController added

---
+++ Test_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Test_API.Models;
+
+namespace Test_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa> {
+                new Villa { Id=1, Name="Pool view"},
+                new Villa { Id = 2, Name="Beach view" }
+            };
+        }
+    }
+}
+
---
+++ Test_API/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Test_API.Models
+{
+	public class Villa
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
+
