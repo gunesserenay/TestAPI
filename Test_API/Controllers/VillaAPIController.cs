567d9bbd5cb00c385c0f643d4350f005505d7ae2
Serenay <serenayygunes@gmail.com>
2023-10-15 21:09:42 +0300

    Response types added

--- Test_API/Controllers/VillaAPIController.cs
+++ Test_API/Controllers/VillaAPIController.cs
@@ -6,28 +6,35 @@ using Test_API.Models.Dto;
 
 namespace Test_API.Controllers
 {
-   // [Route("api/[controller]")]
+    // [Route("api/[controller]")]
     [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return Ok(VillaStore.VillaList); 
+            return Ok(VillaStore.VillaList);
         }
 
-      //  [HttpGet("id")]
+        //  [HttpGet("id")]
         [HttpGet("{id:int}")]
+        //[ProducesResponseType(200)]
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
-            if (id==0)
+            if (id == 0)
             {
                 return BadRequest();
             }
 
             var villa = VillaStore.VillaList.FirstOrDefault(x => x.Id == id);
-            if (villa==null)
+            if (villa == null)
             {
                 return NotFound();
             }
