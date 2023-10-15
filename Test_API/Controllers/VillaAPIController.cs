6cf2a1b8bddc7d9e5e9f184700dcdbab1760e9ef
Serenay <serenayygunes@gmail.com>
2023-10-15 22:43:14 +0300

    HttpDelete in Action

--- Test_API/Controllers/VillaAPIController.cs
+++ Test_API/Controllers/VillaAPIController.cs
@@ -69,6 +69,26 @@ namespace Test_API.Controllers
             VillaStore.VillaList.Add(villaDTO);
             return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
         }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.VillaList.FirstOrDefault(x => x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            VillaStore.VillaList.Remove(villa);
+            return NoContent();
+        }
     }
 }
 
