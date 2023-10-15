a9964ba66eeb9c81ff24a376952096fb4f9e19f9
Serenay <serenayygunes@gmail.com>
2023-10-15 22:07:59 +0300

    Data Annotations added

--- Test_API/Models/Dto/VillaDTO.cs
+++ Test_API/Models/Dto/VillaDTO.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Test_API.Models.Dto
 {
 	public class VillaDTO
 	{
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
     }
 }
