27b62fd4952569c99aa04874c04117a993eda13d
Serenay <serenayygunes@gmail.com>
2023-10-17 17:38:46 +0300

    Entity Framework Core Database models

--- Test_API/Models/Dto/VillaDTO.cs
+++ Test_API/Models/Dto/VillaDTO.cs
@@ -10,9 +10,13 @@ namespace Test_API.Models.Dto
         [MaxLength(100)]
         public string Name { get; set; }
 
-        public int Occupancy { get; set; }
-
+        public string Details { get; set; }
+        [Required]
+        public double Rate { get; set; }
         public int Sqft { get; set; }
+        public int Occupancy { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenity { get; set; }
     }
 }
 
