d45170e4ee4eedca585385c763e282542d9dbb1d
Serenay <serenayygunes@gmail.com>
2023-10-17 18:35:24 +0300

    Use connection string

--- Test_API/Data/ApplicationDbContext.cs
+++ Test_API/Data/ApplicationDbContext.cs
@@ -6,7 +6,13 @@ namespace Test_API.Data
 {
 	public class ApplicationDbContext:DbContext
 	{
-		public DbSet<Villa> Villas { get; set; }
-	}
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+
 }
 
