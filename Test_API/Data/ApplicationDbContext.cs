85d329d420ee3f6bd3a986d3bd8fce068f134152
Serenay <serenayygunes@gmail.com>
2023-10-17 18:03:10 +0300

    Connection string and DbSet Entity

---
+++ Test_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Test_API.Models;
+
+namespace Test_API.Data
+{
+	public class ApplicationDbContext:DbContext
+	{
+		public DbSet<Villa> Villas { get; set; }
+	}
+}
+
