419ad0f042bebc3fab15fee3415b6d7c34484863
Serenay <serenayygunes@gmail.com>
2023-10-20 23:26:24 +0300

    Create Villa table on azure data studio with migration

--- Test_API/Data/VillaStore.cs
+++ Test_API/Data/VillaStore.cs
@@ -3,12 +3,12 @@ using Test_API.Models.Dto;
 
 namespace Test_API.Data
 {
-	public static class VillaStore
-	{
-		public static List<VillaDTO> VillaList = new List<VillaDTO> {
+    public static class VillaStore
+    {
+        public static List<VillaDTO> VillaList = new List<VillaDTO> {
                 new VillaDTO { Id=1, Name="Pool view",Occupancy=4,Sqft=100},
                 new VillaDTO { Id = 2, Name="Beach view",Occupancy=3,Sqft=300}
             };
-	}
+    }
 }
 
