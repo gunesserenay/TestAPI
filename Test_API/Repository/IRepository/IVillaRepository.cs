e3d1e86eaffc3dd2d55f78688611906ac84a219b
Serenay <serenayygunes@gmail.com>
2023-10-22 18:11:43 +0300

    Generic Repository added

--- Test_API/Repository/IRepository/IVillaRepository.cs
+++ Test_API/Repository/IRepository/IVillaRepository.cs
@@ -4,14 +4,11 @@ using Test_API.Models;
 
 namespace Test_API.Repository.IRepository
 {
-	public interface IVillaRepository
-	{
-		Task<List<Villa>> GetAllAsync(Expression<Func<Villa,bool>> filter=null);
-		Task<Villa> GetAsync(Expression<Func<Villa,bool>> filter=null, bool tracked=true);
-		Task CreateAsync(Villa entity);
-        Task RemoveAsync(Villa entity);
-        Task UpdateAsync(Villa entity);
-		Task SaveAsync();
-	}
+    public interface IVillaRepository : IRepository<Villa>
+    {
+
+        Task<Villa> UpdateAsync(Villa entity);
+
+    }
 }
 
